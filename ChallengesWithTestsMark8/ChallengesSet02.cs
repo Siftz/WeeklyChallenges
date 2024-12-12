b4fc73e3a06a81cbf50742a423a0766225759cd7
Logan Akins <lakins2@gmail.com>
2024-12-12 17:44:59 -0500

    2nd Week Challenges Completion

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,52 +8,131 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            ;
+            return char.IsLetter(c);
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            if (vals.Length % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            if (num % 2 == 0) //could also work with (num % 2!=0) then use true if and false else
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
 
-        public double SumOfMinAndMax(IEnumerable<double> numbers)
+        public double SumOfMinAndMax(IEnumerable<double> numbers)//specifies how a collection should behave
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+
+            if (numbers.Count() == 0)
+            {
+                return 0;
+            }
+            return numbers.Min() + numbers.Max();
         }
 
-        public int GetLengthOfShortestString(string str1, string str2)
+        public int GetLengthOfShortestString(string str1, string str2)// compare with greater than and less than
         {
-            throw new NotImplementedException();
+            if (str1.Length < str2.Length)
+            {
+                return str1.Length;
+            }
+            else
+            {
+                return str2.Length;
+            }
         }
 
-        public int Sum(int[] numbers)
+        public int Sum(int[] numbers) // has null possibility
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+            var trueSum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                trueSum += numbers[i];
+            }
+            return trueSum;
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null) // has null possibility 
+            {
+                return 0;
+            }
+            var evenSum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    evenSum += numbers[i];
+                }
+            }
+            //need return sum;
+            return evenSum;
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+             if (numbers == null) //null possible
+             {
+                 return false; // returns a bool not int 
+             }
+             return numbers.Sum() % 2 != 0; 
         }
 
-        public long CountOfPositiveOddsBelowNumber(long number)
+        public long CountOfPositiveOddsBelowNumber(long number) //need a loop for this
         {
-            throw new NotImplementedException();
+            var count = 0;
+
+            if (number == 1 || number <= 0)
+            {
+                return 0;
+            }
+
+            for (long i = number; i > 0; i--)
+            {
+                if (i % 2 == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
         }
+
     }
 }
